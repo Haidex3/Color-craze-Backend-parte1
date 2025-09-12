d47bb2afd25b3c62cc8dd6e72b09db622244beee
Haidex3 <haider.rodriguez-m@mail.escuelaing.edu.co>
2025-09-12 17:20:15 -0500

    add: app configuration

--- src/test/java/com/Color_craze/ColorCrazeApplicationTests.java
+++ src/test/java/com/Color_craze/ColorCrazeApplicationTests.java
@@ -1,13 +1,9 @@
 package com.Color_craze;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ColorCrazeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
