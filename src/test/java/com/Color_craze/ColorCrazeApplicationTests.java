cb3a05a84768aafa79633f1a42cec85305047265
Haidex3 <haider.rodriguez-m@mail.escuelaing.edu.co>
2025-09-12 14:18:57 -0500

    Base Project

---
+++ src/test/java/com/Color_craze/ColorCrazeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Color_craze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorCrazeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
